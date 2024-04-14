05aa7fd33894163cddb1bd16fc45de6d897134c3
Dev <downlaodsnowinx2speed.com>
2024-04-14 13:11:19 +0530

    flashing system added while if user enter wrong username system flash error message

--- models/signup.py
+++ models/signup.py
@@ -2,11 +2,10 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class Todo(db.Model):
+class Signup(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100))
 
     def __init__(self,name):
         self.name = name
    
-
